519fa7d1f301dd93e0d3b2936839cb6bbe7b7205
INDIA GUY <rguy129@yahoo.com>
2019-02-03 16:49:41 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+
+function input(x){
+    document.getElementById("display").value+=x;
+   
+}
+
+
+function result(){
+    var x = document.getElementById("display").value;
+
+    document.getElementById("display").value = eval(x);
+}
+
+ 
+
+//  function avg(){
+    
+//   document.getElementById("display").value = (result / result.length) ;
+//  }
+//     document.getElementById("display").value = eval(x /y)/.length;
+
+// function avg(){
+// var amtOfNumbers = []
+// while (amtOfNumbers = 0, amtOfNumbers >= 0, amtOfNumbers++)
+// document.getElementById("display").value = result.length / result;
+// }
+
